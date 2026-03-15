0ac11cfe8b8e4e00fa53f39cec09f9effc71a8c7
huangrunhui <huangrunhui@192.168.5.26>
2026-03-15 17:59:37 +0800

    feat: Explorer uses filename instead of frontmatter title

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -52,7 +52,14 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-    Component.Explorer(),
+    Component.Explorer({
+    mapFn: (node) => {
+      if (!node.isFolder) {
+        node.displayName = node.slugSegment ?? node.displayName
+      }
+      return node
+    },
+  }),
   ],
   right: [
     Component.Graph(),
@@ -77,7 +84,14 @@ export const defaultListPageLayout: PageLayout = {
         { Component: Component.Darkmode() },
       ],
     }),
-    Component.Explorer(),
+    Component.Explorer({
+    mapFn: (node) => {
+      if (!node.isFolder) {
+        node.displayName = node.slugSegment ?? node.displayName
+      }
+      return node
+    },
+  }),
   ],
   right: [],
 }
